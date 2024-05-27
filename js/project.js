52980460cf739a0046c0bf60129858b4e6ffdc3b
Álvaro de Francisco <alfrasan@gmail.com>
2024-05-27 19:03:03 +0200

    Add coloring event

--- js/project.js
+++ js/project.js
@@ -4,8 +4,12 @@ const gridContainer = document.createElement("div");
 gridContainer.setAttribute("id", "grid-container");
 body.appendChild(gridContainer);
 
-for (let i = 0; i <= (16 * 16) - 1; i++) {
+for (let i = 0; i <= 16 * 16 - 1; i++) {
   let container = document.createElement("div");
   container.setAttribute("class", "container");
+  container.addEventListener(
+    "mouseover",
+    () => (container.style.backgroundColor = "black")
+  );
   gridContainer.appendChild(container);
 }
